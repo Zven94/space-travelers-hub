dff5c3588854492066e63d9e8eab64e84337a65e
Zven94 <nicobolanez@gmail.com>
2023-07-25 19:23:48 -0300

    displaying API data on missions section

--- src/components/Mission/MissionComponent.jsx
+++ src/components/Mission/MissionComponent.jsx
@@ -1,9 +1,45 @@
 import './MissionComponent.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../Navbar/Navbar';
+import { missionsData } from '../../redux/Missions/missionSlice';
 
 function MissionComponent() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+
+  useEffect(() => {
+    dispatch(missionsData());
+  }, [dispatch]);
+
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <div>
+        <h2>Missons</h2>
+        <table>
+          <tr>
+            <th>Missions</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+          {missions?.map((mission) => (
+            <tr key={mission.id}>
+              <td>{mission.name}</td>
+              <td>{mission.description}</td>
+              <td>{mission.status ? 'Active' : 'Inactive'}</td>
+              <td><button type="submit">submit</button></td>
+            </tr>
+          ))}
+        </table>
+        {/* {missions.map((missions) => (
+          <li key={missions.id}>
+            <p>{missions.name}</p>
+          </li>
+        ))} */}
+      </div>
+    </>
   );
 }
 
--- src/redux/Missions/missionSlice.jsx
+++ src/redux/Missions/missionSlice.jsx
@@ -1,12 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const missionUrl = 'https://api.spacexdata.com/v3/missions';
+
+export const missionsData = createAsyncThunk('missions/missionsData', async () => {
+  const response = await axios.get(`${missionUrl}`);
+  return response.data;
+});
 
 const initialState = {
-  missionsArr: [],
+  missions: [],
+  isLoading: false,
+  error: false,
 };
 
 const missionSlice = createSlice({
   name: 'missions',
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(missionsData.fulfilled, (state, action) => ({
+        ...state,
+        missions: action.payload.map((mission) => ({
+          id: mission.missions_id,
+          name: mission.mission_name,
+          description: mission.description,
+          status: false,
+        })),
+      }));
+  },
 });
 
 export default missionSlice.reducer;
