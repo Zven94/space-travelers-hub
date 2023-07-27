9e9fa138c6db2d5581ad66f915b584be68cf04a2
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-27 17:55:49 +0200

    Add files

--- src/components/Rockets/RocketsComponent.jsx
+++ src/components/Rockets/RocketsComponent.jsx
@@ -1,9 +1,18 @@
 import Navbar from '../Navbar/Navbar';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets, selectRocket } from '';
+import '../assests/RocketsComponent.css';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const status = useSelector((state) => state.rockets.status);
 
 function RocketsComponent() {
   return (
     <Navbar />
   );
 }
-
+}
 export default RocketsComponent;
--- src/redux/configureStore.jsx
+++ src/redux/configureStore.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducers from './Missions/missionSlice';
+import RocketsComponent from './Rockets/RocketsComponent.jsx'
 
 export default configureStore({
   reducer: {
