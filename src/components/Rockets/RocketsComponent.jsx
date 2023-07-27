ccef01d3497f61f4e12c6025350660c670e5d1df
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-28 00:39:58 +0200

    Add API

--- src/components/Rockets/RocketsComponent.jsx
+++ src/components/Rockets/RocketsComponent.jsx
@@ -1,18 +1,48 @@
-import Navbar from '../Navbar/Navbar';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets, selectRocket } from '';
-import '../assests/RocketsComponent.css';
+import { fetchRockets, selectRocket } from '../Redux/rockets/rocketsSlice.';
+import '.\Rockets\RocketsComponent.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
   const status = useSelector((state) => state.rockets.status);
 
-function RocketsComponent() {
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, status]);
+  useEffect(() => {
+    if (rockets.length > 0) {
+      dispatch(selectRocket(rockets[0]));
+    }
+  }, [dispatch, rockets]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return <div>Failed to load rocket data</div>;
+  }
+
   return (
-    <Navbar />
+    <section className="rocket-container">
+      <ul className="rocket-list flex">
+        {rockets.map((rocket) => (
+          <li key={rocket.id}>
+            <img src={rocket.flickr_images[0]} className="rocket-img" alt={rocket.name} />
+            <div className="rocket-textBox flex">
+              <h3 className="rocket-name">{rocket.name}</h3>
+              <p className="rocket-description">{rocket.description}</p>
+              <button type="button" className="reserveBttn">Reserve Rocket</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
-}
-}
-export default RocketsComponent;
+};
+
+export default Rockets;
--- src/redux/configureStore.jsx
+++ src/redux/configureStore.jsx
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducers from './Missions/missionSlice';
-import RocketsComponent from './Rockets/RocketsComponent.jsx'
+import RocketsComponent from './rockets/RocketsComponent.jsx'
 
 export default configureStore({
   reducer: {
     missions: missionsReducers,
+    rockets: rocketsReducer,
   },
 });
