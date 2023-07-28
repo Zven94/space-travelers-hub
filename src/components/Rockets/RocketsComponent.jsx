9583ea7412b0c98d516d9c0139bfe600f3e91963
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-28 02:39:43 +0200

    Update RocketsComponent.jsx

--- src/components/Rockets/RocketsComponent.jsx
+++ src/components/Rockets/RocketsComponent.jsx
@@ -1,59 +1,14 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchRockets, selectRocket } from '../../redux/rockets/rocketsSlice';
-
-
-// const Rockets = () => {
-//   const dispatch = useDispatch();
-//   const rockets = useSelector((state) => state.rockets.rockets);
-//   const status = useSelector((state) => state.rockets.status);
-
-//   useEffect(() => {
-//     if (status === 'idle') {
-//       dispatch(fetchRockets());
-//     }
-//   }, [dispatch, status]);
-//   useEffect(() => {
-//     if (rockets.length > 0) {
-//       dispatch(selectRocket(rockets[0]));
-//     }
-//   }, [dispatch, rockets]);
-
-//   if (status === 'loading') {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (status === 'failed') {
-//     return <div>Failed to load rocket data</div>;
-//   }
-
-//   return (
-//     <section className="rocket-container">
-//       <ul className="rocket-list flex">
-//         {rockets.map((rocket) => (
-//           <li key={rocket.id}>
-//             <img src={rocket.flickr_images[0]} className="rocket-img" alt={rocket.name} />
-//             <div className="rocket-textBox flex">
-//               <h3 className="rocket-name">{rocket.name}</h3>
-//               <p className="rocket-description">{rocket.description}</p>
-//               <button type="button" className="reserveBttn">Reserve Rocket</button>
-//             </div>
-//           </li>
-//         ))}
-//       </ul>
-//     </section>
-//   );
-// };
-
-// export default Rockets;
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets, selectRocket } from '../../redux/rockets/rocketsSlice';
 import Navbar from '../Navbar/Navbar';
+
+
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
   const status = useSelector((state) => state.rockets.status);
+
   useEffect(() => {
     if (status === 'idle') {
       dispatch(fetchRockets());
@@ -64,12 +19,15 @@ const Rockets = () => {
       dispatch(selectRocket(rockets[0]));
     }
   }, [dispatch, rockets]);
+
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
+
   if (status === 'failed') {
     return <div>Failed to load rocket data</div>;
   }
+
   return (
     <>
       <Navbar />
@@ -96,4 +54,5 @@ const Rockets = () => {
     </>
   );
 };
+
 export default Rockets;
