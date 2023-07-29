cea7f9d9dd168402af28c3ace533e50f05612d55
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-29 04:02:33 +0200

    Update rocketsSlice.js

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,9 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
-  const response = await axios.get('https://api.spacexdata.com/v3/rockets');
-  return response.data;
+  const response = await fetch('https://api.spacexdata.com/v3/rockets');
+  const data = await response.json();
+  return data;
 });
 
 const rocketsSlice = createSlice({
@@ -55,5 +55,4 @@ const rocketsSlice = createSlice({
 });
 
 export const { selectRocket, cancelReservationRocket, reserveRocket } = rocketsSlice.actions;
-
 export default rocketsSlice.reducer;
