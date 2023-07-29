7d95d3ad7661227084cb443eafc4070f3e3dbcf9
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-29 17:34:04 +0200

    Make change

--- src/components/Mission/ProfileMission.jsx
+++ src/components/Mission/ProfileMission.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 export default function MissionsList() {
   const { missions } = useSelector((store) => store.missions);
   const missionsList = missions.filter((mission) => mission.status);
+  const reservedRocket = useSelector((state) => state.rockets.rockets.filter((rocket) => rocket.reserved === true));
 
   return (
     <>
@@ -16,6 +17,15 @@ export default function MissionsList() {
                 {mission.name}
               </li>
             ))}
+            {reservedRocket.length ? (
+              reservedRocket.map((rocket) => (
+                <li key={rocket.id} className="list-group-item">
+                  {rocket.name}
+                </li>
+              ))
+            ) : (
+              <li className="list-group-item">No Reserved Rockets</li>
+            )}
           </ul>
         )
 
