9521f8484d41d57d98ba7ac93aa1e5679c97d241
Zven94 <nicobolanez@gmail.com>
2023-07-29 11:56:37 -0300

    create two div for profile section

--- src/components/Mission/ProfileMission.jsx
+++ src/components/Mission/ProfileMission.jsx
@@ -5,19 +5,27 @@ export default function MissionsList() {
   const missionsList = missions.filter((mission) => mission.status);
   return (
     <>
-      {
-        missionsList.lenght === 0 ? (
-          <h3>No missions yet</h3>
-        ) : (
-          <ul className="m-0 p-0">
-            {missionsList.map((mission, index) => (
-              <li key={mission.id} className={`list-group-item border ${index % 2 === 0 ? 'bg-primary-subtle' : 'bg-white'}`}>
-                {mission.name}
-              </li>
-            ))}
-          </ul>
-        )
-      }
+      <div>
+        <h2>My Missions</h2>
+        <div>
+          {
+            missionsList.lenght === 0 ? (
+              <h3>No missions yet</h3>
+            ) : (
+              <ul className="m-0 p-0">
+                {missionsList.map((mission, index) => (
+                  <li key={mission.id} className={`list-group-item border ${index % 2 === 0 ? 'bg-primary-subtle' : 'bg-white'}`}>
+                    {mission.name}
+                  </li>
+                ))}
+              </ul>
+            )
+          }
+        </div>
+      </div>
+      <div>
+        <h2>My Rockets</h2>
+      </div>
     </>
   );
 }
