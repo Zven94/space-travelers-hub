50557592b1bce4602e3937dfad233e87a7debe81
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-29 05:50:16 +0200

    Create RocketsComponent.test.jsx

---
+++ src/redux/test/RocketsComponent.test.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from '../configureStore';
+import Rockets from '../../components/Rockets/RocketsComponent';
+
+const mockStore = configureStore([]);
+
+describe('Rockets', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      rockets: {
+        rockets: [
+          {
+            img: 'rocket1.jpg',
+            id: '1',
+            name: 'Falcon 1',
+            description: 'Rocket 1 description',
+            reserved: false,
+          },
+          {
+            img: 'rocket2.jpg',
+            id: '2',
+            name: 'Falcon 9',
+            description: 'Rocket 2 description',
+            reserved: false,
+          },
+        ],
+        status: 'succeeded',
+      },
+    });
+  });
+
+  test('renders rocket list correctly', () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    expect(screen.getByText('Falcon 1')).toBeInTheDocument();
+    expect(screen.getByText('Rocket 1 description')).toBeInTheDocument();
+    expect(screen.getByText('Falcon 9')).toBeInTheDocument();
+    expect(screen.getByText('Rocket 2 description')).toBeInTheDocument();
+  });
+
+  test('displays loading state', () => {
+    store = mockStore({
+      rockets: {
+        rockets: [],
+        status: 'loading',
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  test('displays error state', () => {
+    store = mockStore({
+      rockets: {
+        rockets: [],
+        status: 'failed',
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    expect(screen.getByText('Failed to load rocket data')).toBeInTheDocument();
+  });
+});
